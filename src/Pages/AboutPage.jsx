2ac15c33ce873144e429197d5862715324ff002b
HanitraMelodie <Hanitra_Melodie@hotmail.fr>
2023-08-04 19:29:43 +0100

    ABOUT PAGE CHANGE

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -4,10 +4,8 @@ import CV from "./pdfs/CV.pdf";
 function AboutPage() {
   return (
     <> <h2 classname="page">
-    An enthusiastic educated graduate with an Engineering degree from French
-    Engineering School ESTIA and with a Master of Science in Computer Software
-    and Techniques from Cranfield University. With a committed and curious
-    attitude, I am interested in developing my knowledge and experiences.
+    An enthusiastic educated graduate with an Engineering degree and with a Master of Science in Computer Software
+    and Techniques. With a committed and curious attitude, I am interested in developing my knowledge and experiences.
   </h2>
   <h2 >Please find my <a  href={CV} target="_blank" rel="noreferrer"><span className="aboutmelink">CV here </span></a></h2>
    </>
