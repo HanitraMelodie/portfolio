cad32297a4aeabb5e1d34138cec5f2b2126274d8
HanitraMelodie <Hanitra_Melodie@hotmail.fr>
2023-08-10 12:27:34 +0100

    Correcting Portfolio typo

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,8 +5,8 @@ function HomePage() {
   return (
     <>
     <p></p>
-    <h1>Hello, Welcome to my porfolio </h1>
-    <h1> I am Hanitra and I am a junior full stack developer{" "}</h1>
+    <h1>Hello, Welcome to my portfolio </h1>
+    <h1> I am Hanitra and I am a junior Full stack developer{" "}</h1>
     <img src={logo} alt="Logo" id="profilepicture" />
  </>
     
